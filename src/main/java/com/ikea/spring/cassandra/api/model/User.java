92a3c9ca7d33e935662ce36c31de4dd0b2fffba8
SivaNadupuri <40888140+SivaNadupuri@users.noreply.github.com>
2019-10-21 21:12:16 +0200

    Add files via upload

---
+++ src/main/java/com/ikea/spring/cassandra/api/model/User.java
@@ -0,0 +1,87 @@
+package com.ikea.spring.cassandra.api.model;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table
+
+public class User {
+
+	/**
+	 * @param id
+	 * @param name
+	 * @param address
+	 * @param age
+	 */
+	public User(int id, String name, String address, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.age = age;
+	}
+
+	@PrimaryKey
+	private int id;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	private String name;
+	private String address;
+	private int age;
+
+}
---
+++ src/main/java/com/ikea/spring/cassandra/api/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.ikea.spring.cassandra.api.repository;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.ReactiveCassandraRepository;
+
+import com.ikea.spring.cassandra.api.model.User;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCassandraRepository<User, Integer> {
+
+	@AllowFiltering
+	Flux<User> findByAgeLessThan(int age);
+
+	@AllowFiltering
+	Mono<User> findByAddress(String address);
+
+}
